4c52c0fe2b4246e559e7c3f6eba59eb397edbf7a
Ole Kröger <o.kroeger@opensourc.es>
2019-10-03 14:52:40 +0200

    all_different constraint

--- src/ConstraintSolver.jl
+++ src/ConstraintSolver.jl
@@ -31,13 +31,15 @@ function Base.show(io::IO, csinfo::CSInfo)
     end
 end
 
-mutable struct Constraint
+abstract type Constraint 
+end
+
+mutable struct BasicConstraint <: Constraint
     idx                 :: Int
     fct                 :: Function
     indices             :: Vector{Int}
     pvals               :: Vector{Int}
-    rhs                 :: Int
-    in_all_different    :: Bool # completely part of a all different constraint
+    BasicConstraint() = new()
 end
 
 mutable struct ConstraintOutput
@@ -70,7 +72,7 @@ end
 
 include("Variable.jl")
 include("all_different.jl")
-include("eq_sum.jl")
+# include("eq_sum.jl")
 
 function init()
     com = CoM()
@@ -130,11 +132,14 @@ function fixed_vs_unfixed(search_space, indices)
     return fixed_vals, unfixed_indices
 end
 
-function create_constraint(constraint_idx, fct, variables; rhs = 0)
-    indices = vec([v.idx for v in variables])
-    # will be updated in a later step
-    b_all_different = nameof(fct) == :all_different
-    constraint = Constraint(constraint_idx, fct, indices, Int[], rhs, b_all_different)
+"""
+    set_pvals!(com::CS.CoM, constraint::Constraint)
+
+Computes the possible values inside this constraint and sets it as constraint.pvals
+"""
+function set_pvals!(com::CS.CoM, constraint::Constraint)
+    indices = constraint.indices
+    variables = Variable[v for v in com.search_space[indices]]
     pvals_intervals = Vector{NamedTuple}()
     push!(pvals_intervals, (from = variables[1].from, to = variables[1].to))
     for (i,ind) in enumerate(indices)
@@ -161,20 +166,19 @@ function create_constraint(constraint_idx, fct, variables; rhs = 0)
         pvals = vcat(pvals, collect(interval.from:interval.to))
     end
     constraint.pvals = pvals
-    return constraint
 end
 
 """
-    add_constraint!(com::CS.CoM, fct, variables; rhs=0)
+    add_constraint!(com::CS.CoM, constraint::Constraint)
 
-Add a constraint using a function name and the variables over which the constraint should hold
+Add a constraint to the model
 """
-function add_constraint!(com::CS.CoM, fct, variables; rhs=0)
-    current_constraint_number = length(com.constraints)+1
-    constraint = create_constraint(current_constraint_number, fct, variables; rhs=rhs)
+function add_constraint!(com::CS.CoM, constraint::Constraint)
+    constraint.idx = length(com.constraints)+1
     push!(com.constraints, constraint)
+    set_pvals!(com, constraint)
     for (i,ind) in enumerate(constraint.indices)
-        push!(com.subscription[ind], current_constraint_number)
+        push!(com.subscription[ind], constraint.idx)
     end
 end
 
@@ -516,14 +520,16 @@ end
 
 function set_in_all_different!(com::CS.CoM)
     for constraint in com.constraints
-        if !constraint.in_all_different
-            subscriptions_idxs = [[i for i in com.subscription[v]] for v in constraint.indices]
-            intersects = intersect(subscriptions_idxs...)
+        if :in_all_different in fieldnames(typeof(constraint))
+            if !constraint.in_all_different
+                subscriptions_idxs = [[i for i in com.subscription[v]] for v in constraint.indices]
+                intersects = intersect(subscriptions_idxs...)
 
-            for i in intersects
-                if nameof(com.constraints[i].fct) == :all_different
-                    constraint.in_all_different = true
-                    break
+                for i in intersects
+                    if nameof(com.constraints[i].fct) == :all_different
+                        constraint.in_all_different = true
+                        break
+                    end
                 end
             end
         end
