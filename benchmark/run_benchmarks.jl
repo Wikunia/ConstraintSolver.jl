5473da51e651ae85c5ad6e7a573763df5b8e5a3d
Ole Kröger <o.kroeger@opensourc.es>
2020-05-02 17:33:17 +0200

    run_benchmarks script

---
+++ benchmark/run_benchmarks.jl
@@ -0,0 +1,54 @@
+ 
+#!/usr/bin/env julia
+
+using ArgParse
+
+function parse_commandline()
+    s = ArgParseSettings()
+
+    @add_arg_table! s begin
+        "--base", "-b"
+            help = "Baseline commit id or branch"
+            arg_type = String
+            default = "master"
+        "--target", "-t"
+            help = "Target commit id or branch"
+            arg_type = String
+            required = true
+    end
+
+    return parse_args(s)
+end
+
+
+if isinteractive() == false
+    args = parse_commandline()
+    using PkgBenchmark
+    using ConstraintSolver
+    using GitHub, JSON
+
+    baseline_config = BenchmarkConfig(id = args["base"], juliacmd = `julia -O3`)
+    target_config = BenchmarkConfig(id = args["target"], juliacmd = `julia -O3`)
+    
+    judged = judge("ConstraintSolver", target_config, baseline_config)
+    
+    gist_json = JSON.parse(
+        """
+        {
+        "description": "ConstraintSolver $(args["target"]) vs $(args["base"])",
+        "public": false,
+        "files": {
+            "benchmark.md": {
+            "content": "$(escape_string(sprint(export_markdown, judged)))"
+            }
+        }
+        }
+        """
+    )
+
+    posted_gist = create_gist(params = gist_json);
+    url = get(posted_gist.html_url)
+    println("Gist url: $url")
+end
+
+
