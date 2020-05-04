1f775318ec0c6c18124747446e2dda9b104ac903
Ole Kröger <o.kroeger@opensourc.es>
2020-05-04 11:36:32 +0200

    Bugfix using correct offset in residues + support (#141)

--- src/constraints/table/residues.jl
+++ src/constraints/table/residues.jl
@@ -1,11 +1,11 @@
-function Base.getindex(tr::TableResidues, com::ConstraintSolverModel, var_idx::Int, val::Int)
+function Base.getindex(tr::TableResidues, com::ConstraintSolverModel, var_idx::Int, local_var_idx::Int,  val::Int)
     val_idx = com.search_space[var_idx].init_val_to_index[val+com.search_space[var_idx].offset] 
-    index_shift = tr.var_start[var_idx]-1+val_idx
+    index_shift = tr.var_start[local_var_idx]-1+val_idx
     return tr.values[index_shift]
 end
 
-function Base.setindex!(tr::TableResidues, residue::Int, com::ConstraintSolverModel, var_idx::Int, val::Int)
+function Base.setindex!(tr::TableResidues, residue::Int, com::ConstraintSolverModel, var_idx::Int, local_var_idx::Int, val::Int)
     val_idx = com.search_space[var_idx].init_val_to_index[val+com.search_space[var_idx].offset] 
-    index_shift = tr.var_start[var_idx]-1+val_idx
+    index_shift = tr.var_start[local_var_idx]-1+val_idx
     tr.values[index_shift] = residue
 end
