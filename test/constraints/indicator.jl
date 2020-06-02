8c55f647c5aff25a8c1c1c37f3195b5cf513c9fa
Ole Kröger <o.kroeger@opensourc.es>
2020-06-02 15:25:58 +0200

    more tests

--- test/constraints/indicator.jl
+++ test/constraints/indicator.jl
@@ -1,5 +1,5 @@
 @testset "IndicatorConstraint" begin
-@testset "Basic I" begin
+@testset "Basic ==" begin
     m = Model(CSJuMPTestOptimizer())
     @variable(m, x, CS.Integers([1,2,4]))
     @variable(m, y, CS.Integers([3,4]))
@@ -14,7 +14,7 @@
     @test JuMP.value(y) ≈ 3
 end
 
-@testset "Basic II" begin
+@testset "Basic !=" begin
     m = Model(CSJuMPTestOptimizer())
     @variable(m, x, CS.Integers([1,2,4]))
     @variable(m, y, CS.Integers([3,4]))
@@ -27,5 +27,63 @@ end
     @test JuMP.value(b) ≈ 0.0
     @test JuMP.value(x) ≈ 4
     @test JuMP.value(y) ≈ 4
+end
+
+@testset "Basic >=" begin
+    m = Model(CSJuMPTestOptimizer())
+    @variable(m, x, CS.Integers([1,2,4]))
+    @variable(m, y, CS.Integers([3,4]))
+    @variable(m, b, Bin)
+    @constraint(m, b => {x + y >= 6})
+    @objective(m, Min, x+y)
+    optimize!(m)
+    @test JuMP.termination_status(m) == MOI.OPTIMAL
+    @test JuMP.objective_value(m) ≈ 4.0
+    @test JuMP.value(b) ≈ 0.0
+    @test JuMP.value(x) ≈ 1
+    @test JuMP.value(y) ≈ 3
+
+    m = Model(CSJuMPTestOptimizer())
+    @variable(m, x, CS.Integers([1,2,4]))
+    @variable(m, y, CS.Integers([3,4]))
+    @variable(m, b, Bin)
+    @constraint(m, b == 1)
+    @constraint(m, b => {x + y >= 6})
+    @objective(m, Min, x+y)
+    optimize!(m)
+    @test JuMP.termination_status(m) == MOI.OPTIMAL
+    @test JuMP.objective_value(m) ≈ 6.0
+    @test JuMP.value(b) ≈ 1.0
+    @test JuMP.value(x) ≈ 2
+    @test JuMP.value(y) ≈ 4
+end
+
+@testset "Basic <=" begin
+    m = Model(CSJuMPTestOptimizer())
+    @variable(m, x, CS.Integers([1,2,4]))
+    @variable(m, y, CS.Integers([3,4]))
+    @variable(m, b, Bin)
+    @constraint(m, b => {x + y <= 4})
+    @objective(m, Max, x+y)
+    optimize!(m)
+    @test JuMP.termination_status(m) == MOI.OPTIMAL
+    @test JuMP.objective_value(m) ≈ 8.0
+    @test JuMP.value(b) ≈ 0.0
+    @test JuMP.value(x) ≈ 4
+    @test JuMP.value(y) ≈ 4
+
+    m = Model(CSJuMPTestOptimizer())
+    @variable(m, x, CS.Integers([1,2,4]))
+    @variable(m, y, CS.Integers([3,4]))
+    @variable(m, b, Bin)
+    @constraint(m, b == 1)
+    @constraint(m, b => {x + y <= 4})
+    @objective(m, Max, x+y)
+    optimize!(m)
+    @test JuMP.termination_status(m) == MOI.OPTIMAL
+    @test JuMP.objective_value(m) ≈ 4.0
+    @test JuMP.value(b) ≈ 1.0
+    @test JuMP.value(x) ≈ 1
+    @test JuMP.value(y) ≈ 3
 end
 end
