45cc45e994729fbc966e3d4a362649ad6449b463
Ole Kröger <o.kroeger@opensourc.es>
2020-06-30 15:56:36 +0200

    benchmark

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -25,6 +25,7 @@ solve_eternity("eternity_6x5"; height=6, width=5)
 SUITE["eternity"]["6x5"] = @benchmarkable solve_eternity("eternity_6x5"; height=6, width=5) seconds=30
 SUITE["eternity"]["5x5_opt"] = @benchmarkable solve_eternity("eternity_5x5"; height=5, width=5, optimize=true) seconds=120
 SUITE["eternity"]["5x5_opt_ind"] = @benchmarkable solve_eternity("eternity_5x5"; height=5, width=5, optimize=true, indicator=true) seconds=120
+SUITE["eternity"]["5x5_opt_rei"] = @benchmarkable solve_eternity("eternity_5x5"; height=5, width=5, optimize=true, reified=true) seconds=120
 SUITE["eternity"]["5x5_all"] = @benchmarkable solve_eternity("eternity_5x5"; all_solutions=true) seconds=30
 
 include(joinpath(dir, "benchmark/lp/benchmark.jl"))
