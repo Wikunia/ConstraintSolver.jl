b4ecb79059a42869778fec801da0f7984c39599b
Ole Kröger <o.kroeger@opensourc.es>
2020-11-22 19:31:35 +0100

    new benchmarks for graph coloring

--- benchmark/graph_color/cs.jl
+++ benchmark/graph_color/cs.jl
@@ -1,35 +1,44 @@
-using ConstraintSolver
+using ConstraintSolver, JuMP
 CS = ConstraintSolver
 
-function main(filename; benchmark = false)
-    com = CS.ConstraintSolverModel()
+function main(filename; benchmark = false, time_limit=100)
+    m = Model(optimizer_with_attributes(CS.Optimizer, "time_limit"=>time_limit))
 
     lines = readlines(filename)
     num_colors = 0
     x = nothing
+    max_color = nothing
+    degrees = nothing
     for line in lines
         parts = split(line, " ")
         if parts[1] == "p"
             num_colors = parse(Int, parts[3])
-            x = Vector{CS.Variable}(undef, num_colors)
-            for i = 1:num_colors
-                x[i] = add_var!(com, 1, num_colors)
-            end
+            @variable(m, 1 <= max_color <= num_colors, Int)
+            @variable(m, 1 <= x[1:num_colors] <= num_colors, Int)
+            degrees = zeros(Int, num_colors)
         elseif parts[1] == "e"
             f = parse(Int, parts[2])
             t = parse(Int, parts[3])
-            add_constraint!(com, x[f] != x[t])
+            @constraint(m, x[f] != x[t])
+            degrees[f] += 1
+            degrees[t] += 1
         end
     end
+    max_degree = maximum(degrees)
+
+    println("max_degree: ", max_degree)
     println("num_colors: ", num_colors)
+    @constraint(m, max_color <= max_degree)
 
-    set_objective!(com, :Min, CS.vars_max(x))
+    @constraint(m, max_color .>= x)
+    @objective(m, Min, max_color)
 
-    status = solve!(com)
+    optimize!(m)
+
+    status = JuMP.termination_status(m)
 
     if !benchmark
         println("status: ", status)
-        println("objective: ", com.best_sol)
-        com.info
+        println("objective: ", JuMP.objective_value(m))
     end
 end
