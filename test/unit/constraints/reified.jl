abedfc36096c36fa9bdd406968513a5ea4b8e458
Ole Kröger <o.kroeger@opensourc.es>
2020-12-09 20:14:01 +0100

    reified test is_constraint_violated

---
+++ test/unit/constraints/reified.jl
@@ -0,0 +1,31 @@
+@testset "reified is_constraint_violated test" begin
+    m = Model(optimizer_with_attributes(CS.Optimizer, "no_prune" => true, "logging" => []))
+    @variable(m, b, Bin)
+    @variable(m, -5 <= x[1:5] <= 5, Int)
+    @constraint(m, b := {x in CS.AllDifferentSet()})
+    optimize!(m)
+    com = JuMP.backend(m).optimizer.model.inner
+
+    constraint = com.constraints[1]
+
+    variables = com.search_space
+    @test CS.fix!(com, variables[constraint.indices[1]], 1; check_feasibility = false)
+    @test CS.fix!(com, variables[constraint.indices[2]], 3; check_feasibility = false)
+    @test CS.fix!(com, variables[constraint.indices[3]], 3; check_feasibility = false)
+    @test CS.is_constraint_violated(com, constraint, constraint.fct, constraint.set)
+
+    m = Model(optimizer_with_attributes(CS.Optimizer, "no_prune" => true, "logging" => []))
+    @variable(m, b, Bin)
+    @variable(m, -5 <= x[1:5] <= 5, Int)
+    @constraint(m, b := {x in CS.AllDifferentSet()})
+    optimize!(m)
+    com = JuMP.backend(m).optimizer.model.inner
+
+    constraint = com.constraints[1]
+
+    variables = com.search_space
+    @test CS.fix!(com, variables[constraint.indices[1]], 1; check_feasibility = false)
+    @test CS.fix!(com, variables[constraint.indices[2]], 1; check_feasibility = false)
+    @test CS.fix!(com, variables[constraint.indices[3]], 3; check_feasibility = false)
+    @test !CS.is_constraint_violated(com, constraint, constraint.fct, constraint.set)
+end
