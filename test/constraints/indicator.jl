fff6b9cd054f815818497fb7f4b8924e0848a8a0
Ole Kroeger <o.kroeger@opensourc.es>
2021-07-18 12:46:15 +0200

    test for reified and indicator in indicator

--- test/constraints/indicator.jl
+++ test/constraints/indicator.jl
@@ -270,4 +270,66 @@
         com = CS.get_inner_model(m)
         @test is_solved(com)
     end
+
+    @testset "indicator in indicator" begin
+        m = Model(optimizer_with_attributes(
+            CS.Optimizer,
+            "all_solutions"=> true,
+            "logging" => [],
+        ))
+        @variable(m, a, Bin)
+        @variable(m, b, Bin)
+        @variable(m, c, Bin)
+        @constraint(m, a => { b => { c == 1}})
+        optimize!(m)
+        @test JuMP.termination_status(m) == MOI.OPTIMAL
+        com = CS.get_inner_model(m)
+        nresults = JuMP.result_count(m)
+        results = Set{Tuple{Bool,Bool,Bool}}()
+        for i in 1:nresults
+            aval,bval,cval = convert.(Bool, round.(JuMP.value.([a,b,c]; result=i)))
+            if aval && bval
+                @test cval
+            end
+            push!(results, (aval, bval, cval))
+        end
+        for av in [false, true], bv in [false, true], cv in [false, true]
+            if !av || !bv
+                @test (av,bv,cv) in results
+            elseif av && bv && cv
+                @test (av,bv,cv) in results
+            end
+        end
+    end
+
+    @testset "reified in indicator" begin
+        m = Model(optimizer_with_attributes(
+            CS.Optimizer,
+            "all_solutions"=> true,
+            "logging" => [],
+        ))
+        @variable(m, a, Bin)
+        @variable(m, b, Bin)
+        @variable(m, c, Bin)
+        @constraint(m, a => { b := { c == 1}})
+        optimize!(m)
+        @test JuMP.termination_status(m) == MOI.OPTIMAL
+        com = CS.get_inner_model(m)
+        nresults = JuMP.result_count(m)
+        results = Set{Tuple{Bool,Bool,Bool}}()
+        for i in 1:nresults
+            aval,bval,cval = convert.(Bool, round.(JuMP.value.([a,b,c]; result=i)))
+            if aval 
+                @test (bval && cval) || (!bval && !cval)
+            end
+            push!(results, (aval, bval, cval))
+        end
+        for av in [false, true], bv in [false, true], cv in [false, true]
+            if !av 
+                @test (av,bv,cv) in results
+            elseif (bv && cv) || (!bv && !cv)
+                @test (av,bv,cv) in results
+            end
+        end
+    end
 end
